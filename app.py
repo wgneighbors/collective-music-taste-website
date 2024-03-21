3bd2ba07580867f36140ab3f123e43c328548f67
William Neighbors <130516791+wgneighbors@users.noreply.github.com>
2024-03-21 18:02:18 +0000

    Functional routing, created base.html

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def home():
     return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 print(__name__)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug = True)
+  app.run(host='0.0.0.0', debug = True)
